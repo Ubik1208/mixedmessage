3bec5c75b1565bfe9a2572844dcde881ced76dd7
Lukas <luka.lukasiewicz@gmail.com>
2022-06-06 20:53:40 +0200

    dodano html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //arrays with words example
-const podmiot = ['I', 'you', 'he', 'she', 'we', 'they'];
+const podmiot = ['I', 'You', 'He', 'She', 'We', 'They'];
 const verb = [ 'go', 'do', 'ride', 'visit'];
 const dopelnienie = [ 'home', 'bank', 'school'];
 //pierwsza funkcja będzie się zajmować mieszaniem
