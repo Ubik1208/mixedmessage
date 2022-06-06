d79c0eafafcc5beea4937cd124ae988aa114ca3b
Lukas <luka.lukasiewicz@gmail.com>
2022-06-06 16:29:34 +0200

    dodana funkcja losująca jeden wyraz

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const verb = [ 'go', 'do', 'ride', 'visit'];
 const dopelnienie = [ 'home', 'bank', 'school'];
 //pierwsza funkcja będzie się zajmować mieszaniem
 //druga funkcja będzie składać zdanie - moze mozna wykorzytsać getter
+function randomWord(wordArray) {
+    let chosenWord = wordArray[Math.floor(Math.random()*wordArray.length)];
+    return chosenWord;
+} 
+
+console.log(randomWord(podmiot));
