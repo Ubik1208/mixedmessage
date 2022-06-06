53cf01bd732ac40364eda50e6149ec99bfac86c2
Lukas <luka.lukasiewicz@gmail.com>
2022-06-06 16:15:22 +0200

    poprawione nawiasy

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //arrays with words example
-const podmiot = {'I', 'you', 'he', 'she', 'we', 'they'};
-const verb = { 'go', 'do', 'ride', 'visit'};
-const dopelnienie = { 'home', 'bank', 'school'}
+const podmiot = ['I', 'you', 'he', 'she', 'we', 'they'];
+const verb = [ 'go', 'do', 'ride', 'visit'];
+const dopelnienie = [ 'home', 'bank', 'school'];
+//pierwsza funkcja będzie się zajmować mieszaniem
+//druga funkcja będzie składać zdanie - moze mozna wykorzytsać getter
